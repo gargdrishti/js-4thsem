6175fa11a3bcae20a2fc77e90e788c1786f69f03
Drishti <155943006+gargdrishti@users.noreply.github.com>
2025-02-23 12:15:47 +0000

    comparison

---
+++ basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >=1);
+// console.log(2 > 1);
+// console.log(2 ==1);
+// console.log(2 !=1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+//conparisons convert null to a number,treating it as 0
+console.log(null > 0);
+console.log(null ==0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+console.log("2" === 2);
