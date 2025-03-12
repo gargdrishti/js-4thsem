e357a27356d90288698c7578a0876a40f8c10761
Drishti <155943006+gargdrishti@users.noreply.github.com>
2025-03-12 11:26:09 +0000

    fdv

---
+++ 04_iterations/one.js
@@ -0,0 +1,17 @@
+//for
+//syntax
+// for(let index=0;index<10;index++){
+//     const element = index;
+    //    console.log(element);
+// }
+
+
+//break and continue
+for(let i=0;i<=20;i++)
+{
+    if(i == 5){
+        console.log(`detected 5`);
+        continue
+    }
+    console.log(`value of i is ${i}`);
+}
---
+++ 04_iterations/two.js
@@ -0,0 +1,20 @@
+//while loop
+
+// let index=0;
+// while(index <=10){
+//     console.log(`value of index is ${index}`);
+//     index++;
+// }
+
+let myArray = ['flash',"batman","superman"]
+let arr=0
+while(arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr++;
+}
+
+
+
+//do-while loop
+
+do
